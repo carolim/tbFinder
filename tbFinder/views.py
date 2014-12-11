8d3c42dab37e3cb9af23c9ef05419a0daa8cc7a7
Carolyn Lim <carolim@sas.upenn.edu>
2014-12-11 18:40:12 -0500

    all courses page created

--- tbFinder/views.py
+++ tbFinder/views.py
@@ -3,3 +3,8 @@ from tbFinder import app
 @app.route('/')
 def index():
     return 'first route yeahhhhhhhhh'
+
+@app.route('/allCourses')
+def allCourses():
+    #should show a page with all the current courses
+    return courses!
