bf697cb855b2ce88f4a45fb77c36f829d5d4fb6a
Carolyn Lim <carolim@sas.upenn.edu>
2014-12-14 21:19:15 -0500

    added optional args to runserver.py

--- runserver.py
+++ runserver.py
@@ -1,2 +1,24 @@
 from tbFinder import app
-app.run(debug=True)
+import optparse
+
+def main(options):
+	if options.debug == True:
+		if options.port == "-1":
+			app.run(debug=True)
+		else:
+			app.run(debug=True, port=int(options.port))
+	else:
+		if options.port == "-1":
+			app.run(debug=False)
+		else:
+			app.run(debug=False, port=int(options.port))
+
+
+if __name__ == "__main__":
+	optparser = optparse.OptionParser()
+	optparser.add_option('-d', '--debug', action="store_true", default=False,
+							 dest="debug", help="runs app in debug mode")
+	optparser.add_option('-p', '--setport', action="store", default="-1",
+							 dest="port", help="controls which port the local server runs on")
+	options, args = optparser.parse_args()
+	main(options)
