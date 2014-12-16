0fa4537c723b4fceae1a168d54f4c14959e0e5dc
ericmurray93 <ericmurray93@gmail.com>
2014-12-15 22:35:01 -0500

    Update views.py

--- tbFinder/views.py
+++ tbFinder/views.py
@@ -1,9 +1,9 @@
 import tbFinder.db as db
 import json
+import random
 from tbFinder import app
 from flask import Flask, render_template, redirect, request
 import re
-import random
 
 
 #Homepage
@@ -12,7 +12,7 @@ import random
 def home():
     return render_template('home.html')
 
-#valiadate the 
+#valiadate the email
 @app.route('/valiadate', methods=['POST', 'GET'])
 def valiadate(name = None):
     email = request.form['email'].encode('ascii','ignore')
@@ -28,6 +28,11 @@ def createUser():
 	return User(x)
 
 
+@app.route('/report', methods=['GET', 'POST'])
+def report():
+	return render_template('report.html')
+
+
 @app.route('/allCourses')
 def allCourses():
     all_courses = db.get_all_course_codes()
@@ -36,12 +41,11 @@ def allCourses():
 
 @app.route('/course/<dept>')
 @app.route('/course/<dept>/<course_id>')
-def course(dept, course_id=None):
-	if (course_id==None):
-		results = db.get_all_dept_links(dept)
-		return render_template('dept_results.html', dept=dept,
-								results=results)
-	else:	
+def course(dept, course_id=None, methods =['GET']):
+    if (course_id is None):
+        results = db.get_all_dept_links(dept)
+        return render_template('dept_results.html', dept=dept, results=results)
+    else:	
 		results = db.get_all_course_links(dept, course_id)
 		return render_template('courseid_results.html', dept=dept,
 								course_id=course_id, results=json.loads(results))
@@ -49,47 +53,40 @@ def course(dept, course_id=None):
 
 @app.route('/add')
 def add():
-	return render_template('add.html')
+    return render_template('add.html')
 
 
 @app.route('/addLink', methods=['POST'])
 def addLink():
-	db.add_tb_info(request.form)
-	return redirect('/allCourses')
+    db.add_tb_info(request.form)
+    return redirect('/allCourses')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 @app.route('/explore')
 def explore():
-	results = db.get_all_course_codes()
-	dept_list = list(results.keys())
-	# randomly pick a dept to go to
-	selected_dept = random.choice(dept_list)
-	return redirect('/course/' + selected_dept)
+    results = db.get_all_course_codes()
+    dept_list = list(results.keys())
+    selected_dept = random.choice(dept_list)
+    return redirect('/course/' + selected_dept)
 
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
-	query = request.form.get('query')
-	# make query to db
-	try:
-		query = int(query)
-		results = db.search(query)
-		if len(results) == 0:
-			print "no results found"
-			return render_template('results.html', query=query,
-										results="null")
-		else:
-			return render_template('results.html', query=query,
-										results=results)
-	except ValueError:
-		print 'THIS IS REACHED'
-		return render_template('results.html', query=query,
-										results="notanint")
+    query = request.form.get('query')
+    try:
+        query = int(query)
+        results = db.search(query)
+        if len(results) == 0:
+            return render_template('results.html', query=query, results="null")
+        else:
+            return render_template('results.html', query=query, results=results)
+    except ValueError:
+        return render_template('results.html', query=query, results="notanint")
 
 
 class User():
